04f8154bdbdcce2324466e6acc5a7cacecf0fc60
AlexandraAndersson <Alexandra.Andersson@hevs.ch>
2020-03-24 17:42:46 +0100

    Updated files

--- MATLAB/importFreqData.m
+++ MATLAB/importFreqData.m
@@ -4,9 +4,11 @@ V1=[];
 V2=[];
 for k=0:4
 file = [path 'R10k_' num2str(k) '.csv'];
-data = readmatrix(file);
+data = readmatrix(file, 'Range', 2);
 
 freq(:,k+1) = data(:,1);
 V2(:,k+1) = 10.^(data(:,3)/20).*exp(1j*data(:,4)*pi/180);
+Amp2(:,k+1) = 10.^(data(:,3)/20);
+Phase2(:,k+1) = data(:,4)*pi/180;
 
 end
