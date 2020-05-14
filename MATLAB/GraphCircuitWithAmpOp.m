feb33742b6d1d85db8a1e50b6875dcb832ea125e
Alexandre Figueiredo <alexandrefig.1996@hotmail.com>
2020-05-14 09:00:54 +0200

    Update
    Add some screens, uml files, data

---
+++ MATLAB/GraphCircuitWithAmpOp.m
@@ -0,0 +1,26 @@
+path = 'C:\Users\Alexa\eln1-project-2020-ak-team\data\';
+freq = [];
+V1=[];
+V2=[];
+
+file = [path 'RprCprAmpOp.txt'];
+data = readmatrix(file, 'Range', 2);
+
+% Declare variables
+time = data(:,1);
+Vout = data(:,2);
+
+% Import LT Spice value
+
+lastLine = 2982;
+stepInds = [0;find(isnan(data(:,1)));lastLine];
+for k=1:(size(stepInds,1)-1)
+    dta = data(stepInds(k)+1:stepInds(k+1)-1,:);
+    t0 = dta(1,1);
+    nSamples = 500;
+    tstep = (dta(end,1)-dta(1,1))/nSamples;
+    t1 = (t0:(tstep):dta(end,1))';
+
+    V1(:,k) = interp1(dta(:,1), dta(:,2), t1);
+    plot(V1);
+end
